b9fc7a9e93dab55e16f26c91e1e7f6b7e273612f
duhaime <douglas.duhaime@gmail.com>
2019-07-12 14:46:55 -0400

    add function for processing glob of files

--- wordmap/__init__.py
+++ wordmap/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
-from wordmap.wordmap import plot, \
+from wordmap.wordmap import parse, \
+  plot, \
   plot_gensim_word2vec, \
   prepare_web_assets, \
   serve
--- wordmap/wordmap.py
+++ wordmap/wordmap.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify, request, send_from_directory, render_template
-from MulticoreTSNE import MulticoreTSNE as TSNE
+from sklearn.manifold import TSNE
+from gensim.models import Word2Vec
 from flask_cors import CORS
 from os.path import join
+from umap import UMAP
 import numpy as np
 import rasterfairy
 import gensim
+import codecs
 import shutil
+import glob
 import json
+import sys
 import os
+import re
 
 
 # Store a reference to the location of this package
@@ -20,7 +26,7 @@ cwd = os.getcwd()
 web_target = join(cwd, 'web')
 
 
-def plot(words, df, max_words=100000):
+def plot(words, df, max_words=100, n_components=2):
   '''
   Project word vectors in a numpy array into 2D space
   and persist the resulting data structures for viewing.
@@ -34,18 +40,20 @@ def plot(words, df, max_words=100000):
     This dataframe should have shape len(words), k, where k is some
     integer. Each row in the dataframe should indicate the position
     of a word in some vector space (e.g. Word2Vec, NMF, GloVe, LDA...)
+  n_components : int
+    The number of dimensions to use in the embedding space
   max_words : int
     The maximum number of words to include in the visualization. The more
     words, the greater the number of primitives WebGL has to draw (each letter
     of each word is drawn as a single GL_POINT).
   '''
-  # get initial point assignments from fast TSNE
-  print(' * creating TSNE layout')
-  t_proj = TSNE(n_jobs=8).fit_transform(df)
-  # combine the words with the TSNE projections
+  # get initial point assignments
+  print(' * creating UMAP layout')
+  t_proj = UMAP(n_components=n_components).fit_transform(df)
+  # combine the words with the projections
   t = [[words[idx], int(i[0]), int(i[1])] for idx, i in enumerate(t_proj)]
   # use linear assignment to gridify points; r_proj is discretized
-  print(' * quantizing TSNE layout')
+  print(' * quantizing layout')
   r_proj = rasterfairy.transformPointCloud2D(t_proj)
   # combine the words and positions
   r = [[words[idx], int(i[0]), int(i[1])] for idx, i in enumerate(r_proj[0])]
@@ -55,7 +63,7 @@ def plot(words, df, max_words=100000):
       i[2] += 0.5
   # format the layout json
   layouts = [
-    {'words': words, 'positions': t, 'name': 'tsne'},
+    {'words': words, 'positions': t, 'name': 'umap'},
     {'words': words, 'positions': r, 'name': 'grid'},
   ]
   # copy web assets to user's location
@@ -66,18 +74,6 @@ def plot(words, df, max_words=100000):
   out_path = join(web_target, 'wordmap-layouts.json')
   with open(out_path, 'w') as out:
     json.dump(layouts, out)
-
-
-def prepare_web_assets():
-  '''
-  Copy the web assets required for rendering to the user's cwd
-  '''
-  print(' * preparing web assets')
-  if not os.path.exists(web_target):
-    shutil.copytree(join(path, 'web'), web_target)
-  else:
-    print(' ! cowardly refusing to copy web assets to', web_target)
-    print(' !', web_target, 'is not empty')
 
 
 def plot_gensim_word2vec(model, max_words=100000):
@@ -100,6 +96,42 @@ def plot_gensim_word2vec(model, max_words=100000):
     print('! Number of words:', len(words))
     print('! Number of elements in dataframe:', df.shape[0])
   plot(words, df)
+
+
+def parse(encoding='utf8', size=100000, window=5, min_count=5, workers=4):
+  '''
+  Main method for parsing a glob of files passed on the command line
+  '''
+  if len(sys.argv) < 2:
+    print('\nPlease identify a glob of files to process:\n')
+    print('wordmap "my_data/*.txt"')
+    sys.exit()
+  files = glob.glob(sys.argv[1])
+  if len(files) < 1:
+    print('\nYour specified file list is empty. Please check your file glob.\n')
+    sys.exit()
+  display_files = files[:10] + ['...'] if len(files) >= 10 else files
+  sep = '    \n    '
+  print(' * preparing to parse {0} files:{1}'.format(len(files), sep + sep.join(display_files)))
+  word_lists = []
+  for i in files:
+    with codecs.open(i, 'r', encoding) as f:
+      word_lists.append(re.sub(r'[^\w\s]','',f.read().lower()).split())
+  print(' * building model')
+  model = Word2Vec(word_lists, size=size, window=window, min_count=min_count, workers=workers)
+  model.save('word2vec.model')
+  plot_gensim_word2vec(model)
+
+
+def prepare_web_assets():
+  '''
+  Copy the web assets required for rendering to the user's cwd
+  '''
+  print(' * preparing web assets')
+  if not os.path.exists(web_target):
+    shutil.copytree(join(path, 'web'), web_target)
+  else:
+    print(' !', web_target, 'is not empty; assets not copied')
 
 
 def serve():
